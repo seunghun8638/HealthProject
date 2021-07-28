4c87e57a546bc4907df92bcfa737e3ef0f8c5bc2
seunghun <tmdgnsdl23@naver.com>
2021-07-28 11:52:10 +0900

    Initial Commit

---
+++ HealthProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthProject
+//
+//  Created by 오승훈 on 2021/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
